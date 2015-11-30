89bbbba7f99ca5d8e252bdad9fd87f181c4fbb54
Thomas Güttler <tguettler@tbz-pariv.de>
2015-11-30 16:22:51 +0100

    changed version from 2015.4 to 2015.5 in setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 setuptools.setup(
     name='reprec',
-    version='2015.4',
+    version='2015.5',
     license='BSD',
     url='https://github.com/guettli/reprec',
     long_description=open('README.rst').read(),
